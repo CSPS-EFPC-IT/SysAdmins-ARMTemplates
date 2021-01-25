485a7690209e01a6f61b95a171ea55197b648463
LPThibert-CA <57185578+LPThibert-CA@users.noreply.github.com>
2021-01-25 16:43:02 -0500

    Update ArmOutputsScript.ps1
    Added output and improved formatting.

--- Scripts/ArmOutputsScript.ps1
+++ Scripts/ArmOutputsScript.ps1
@@ -8,15 +8,19 @@ param (
 #Convert string into objects
 $armOutputObj =  $armOutputString | convertfrom-json
 
-    #Loops through objects to create variables
-	$armOutputObj.PSObject.Properties | ForEach-Object {
-		$type = ($_.value.type).ToLower()
-		$key = $_.name
-		$value = $_.value.value
+#Loops through objects to create variables
+$armOutputObj.PSObject.Properties | ForEach-Object {
+	$type = ($_.value.type).ToLower()
+	$key = $_.name
+	$value = $_.value.value
 
-		if ($type -eq "securestring") {
-			Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value"
-		} elseif ($type -eq "string") {
-			Write-Host "##vso[task.setvariable variable=$key]$value"
-		}
+	if ($type -eq "securestring") {
+		Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value"
+		$outPut = "Secure variable '" + $key + "' was created."
+		Write-Host $outPut
+	} elseif ($type -eq "string") {
+		Write-Host "##vso[task.setvariable variable=$key]$value"
+		$outPut = "Variable '" + $key + "' was created with the following value: '" + $value + "'"
+		Write-Host $outPut
 	}
+}
